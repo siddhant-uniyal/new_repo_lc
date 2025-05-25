f4ff161f3e0f5f09457e9e05733200844deb5486
Siddhant Uniyal <110982730+siddhant-uniyal@users.noreply.github.com>
2025-05-25 22:43:52 +0530

    3815. Sum of Largest Prime Substrings | Beats 0% Time 0% Space

---
+++ math/3815-sum-of-largest-prime-substrings/3815-sum-of-largest-prime-substrings-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    long long sumOfLargestPrimes(string s) {
+        int n = s.size();
+        map<long,int>mp;
+        for(int i = 0 ; i < n ; ++i){
+            long num = 0;
+            for(int j = i ; j < n ; ++j){
+                num = num*10L + (s[j] - '0');
+                bool isPrime = true;
+                for(int k = 2 ; k * k <= num ; ++k){
+                    if(num%k == 0){
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if(isPrime && num != 1) mp[num] = 1;
+            }
+        }
+        if(mp.empty()) return 0LL;
+        auto it = prev(mp.end());
+        long long ans = 0LL;
+        for(int i = 0 ; i < 3 ; ++i){
+            ans += it->first;
+            if(it == mp.begin()) break;
+            --it;
+        }
+        return ans;
+    }
+};
