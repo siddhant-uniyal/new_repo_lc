a7fbd63ac0a8817ba57f19f8964f9ce9451c9aa9
Siddhant Uniyal <110982730+siddhant-uniyal@users.noreply.github.com>
2025-05-25 10:12:12 +0530

    2237. Longest Palindrome by Concatenating Two Letter Words | Beats 56% Time 8% Space

---
+++ string/2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        /*
+        xy -> yx or xx -> xx
+        and xx in the middle
+        */
+        int ans = 0;
+        map<int,int>mp;
+        auto hash = [&](char x , char y) -> int{
+            return (x - 'a') * 26 + y - 'a';
+        };
+        for(auto &s : words){
+            int req = hash(s[1] , s[0]);
+            if(mp.count(req)){
+                ans += 4;
+                --mp[req];
+                if(mp[req] == 0) mp.erase(req);
+            }
+            else ++mp[hash(s[0] , s[1])];
+        }
+        for(auto &[k , v] : mp){
+            if(k/26 == k%26){
+                ans += 2;
+                break;
+            }
+        }
+        return ans;
+    }
+};
