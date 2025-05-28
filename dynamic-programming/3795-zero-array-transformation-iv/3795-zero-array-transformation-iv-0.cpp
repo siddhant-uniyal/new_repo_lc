b33b5074ad3a2e228c02a178c8a7a3860e21a394
Siddhant Uniyal <110982730+siddhant-uniyal@users.noreply.github.com>
2025-05-28 21:54:32 +0530

    3795. Zero Array Transformation IV | Beats 43% Time 59% Space

---
+++ dynamic-programming/3795-zero-array-transformation-iv/3795-zero-array-transformation-iv-0.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+        int sz = queries.size();
+        int l = 0 , r = sz + 1;
+        while(l < r){
+            int m = l + ((r-l)>>1);
+            bool ok = true;
+            vector<vector<int>>vals(n);
+            for(int i = 0 ; i < m ; ++i){
+                for(int j = queries[i][0] ; j <= queries[i][1] ; ++j){
+                    vals[j].push_back(queries[i][2]);
+                }
+            }
+            for(int i = 0 ; i < n ; ++i){
+                if(!nums[i]) continue;
+                int valCnt = vals[i].size();
+                vector<vector<bool>>dp(valCnt + 1 , vector<bool>(nums[i] + 1 , false));
+                dp[0][0] = true;
+                for(int j = 1 ; j <= valCnt ; ++j){
+                    for(int k = 0 ; k <= nums[i] ; ++k){
+                        bool fill = dp[j - 1][k];
+                        if(vals[i][j - 1] <= k){
+                            fill |= dp[j - 1][k - vals[i][j-1]];
+                        }
+                        dp[j][k] = fill;
+                    }
+                }
+                if(!dp[valCnt][nums[i]]){
+                    ok = false;
+                    break;
+                }
+            }
+            if(ok) r = m;
+            else l = m + 1;
+        }
+        return l == sz + 1 ? -1 : l;
+    }
+};
