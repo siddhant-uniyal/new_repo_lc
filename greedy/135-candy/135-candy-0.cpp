0e7d3d78662fca3c218589d7e39174e7bcc37833
Siddhant Uniyal <110982730+siddhant-uniyal@users.noreply.github.com>
2025-06-03 11:46:35 +0530

    135. Candy | Beats 0% Time 0% Space

---
+++ greedy/135-candy/135-candy-0.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        /*
+        if array is strictly inc/dec : just do 1,2,.n or reverse
+        so just divide into strictly inc runs , by default its : 1,2,..len , but if element before this run's first elem < , add prevCandy*len
+        */
+        int n = ratings.size();
+        int ans = 0;
+        int prevCandy = 0;
+        int prevLen = 0;
+        for(int i = 0 ; i < n ; ){
+            int j = i + 1;
+            while(j < n && ratings[j] > ratings[j - 1]) ++j;
+            // cout << i << " " << j << endl;
+            int len = j - i;
+            ans += len * (len + 1) / 2;
+            int prevRating = i ? ratings[i - 1] : INT_MAX;
+            if(ratings[i] > prevRating){
+                ans += prevCandy * len;
+                prevCandy += len;
+            }
+            else{
+                prevCandy = len;
+                if(ratings[i] < prevRating) ans += (prevLen == 1);
+            }
+            prevLen = len;
+            i = j;
+        }
+        // 1 3 2 2 1
+        // 1 2 1 1 2
+        return ans;
+    }
+};
