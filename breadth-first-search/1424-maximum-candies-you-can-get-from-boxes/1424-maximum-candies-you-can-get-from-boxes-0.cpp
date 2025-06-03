981f79fc9c70833ec563689952643152180ceadc
Siddhant Uniyal <110982730+siddhant-uniyal@users.noreply.github.com>
2025-06-03 11:19:19 +0530

    1424. Maximum Candies You Can Get from Boxes | Beats 59% Time 31% Space

---
+++ breadth-first-search/1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes-0.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
+        int n = status.size();
+        vector<int>haveKey(n); 
+        set<int>noKeyBoxes;
+        queue<int>bfs;
+        for(int i : initialBoxes){
+            if(status[i] == 1){
+                bfs.push(i);
+            }
+            else noKeyBoxes.insert(i);
+        }
+        int ans = 0;
+        while(!bfs.empty()){
+                int src = bfs.front();
+                bfs.pop();
+                ans += candies[src];
+                for(int j : keys[src]){
+                    haveKey[j] = 1;
+                    if(noKeyBoxes.count(j)){
+                        bfs.push(j);
+                        noKeyBoxes.erase(j);
+                    }
+                }
+                for(int j : containedBoxes[src]){
+                    if(status[j] == 1 || haveKey[j] == 1){
+                        bfs.push(j);
+                    }
+                    else noKeyBoxes.insert(j);
+                }
+        }
+        return ans;
+    }
+};
