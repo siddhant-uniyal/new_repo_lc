d348857deef59c342f7ceb6f9f8f2b29c6a803c6
Siddhant Uniyal <110982730+siddhant-uniyal@users.noreply.github.com>
2025-06-07 09:15:09 +0530

    3445. Lexicographically Minimum String After Removing Stars | Beats 0% Time 0% Space

---
+++ heap-priority-queue/3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars-0.cpp
@@ -0,0 +1,34 @@
+int occ[26];
+class Solution {
+public:
+    string clearStars(string s) {
+        int n = s.size();
+        memset(occ , 0 , sizeof(occ));
+        auto getMn = [&](int *occ) -> char{
+            for(int i = 0 ; i < 26 ; ++i){
+                if(occ[i]) return 'a' + i;
+            }
+            return 'z' + 1;
+        };
+        string ans = "";
+        for(int i = 0 ; i < n ; ){
+            int j = i;
+            while(j < n && s[j] != '*') ++occ[s[j] - 'a'] , ++j;
+            // cout << i << " " << j << endl;
+            char mn = getMn(occ);
+            bool first = (j < n);
+            string temp = "";
+            for(int k = j - 1 ; k >= i ; --k){
+                if(s[k] == mn && first){
+                    first = false;
+                    continue;
+                }
+                temp += s[k];
+            }
+            reverse(temp.begin() , temp.end());
+            ans += temp;
+            i = j + 1;
+        }
+        return ans;
+    }
+};
