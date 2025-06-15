07adc3a8fcda663f23bdd3795a9ff66b56975268
Siddhant Uniyal <110982730+siddhant-uniyal@users.noreply.github.com>
2025-06-15 13:41:08 +0530

    1929. Maximum Value at a Given Index in a Bounded Array | Beats 0% Time 0% Space

---
+++ binary-search/1929-maximum-value-at-a-given-index-in-a-bounded-array/1929-maximum-value-at-a-given-index-in-a-bounded-array-0.cpp
@@ -0,0 +1,29 @@
+using ll = long long;
+class Solution {
+public:
+    int maxValue(int n, int index, int maxSum) {
+        /* 
+        bs : lower bound : 1
+        upper bound : just keep loose as maxSum
+
+        then greedy 
+        */
+        auto getSum = [&](ll n , ll m) -> ll{
+            ll res = n * (n + 1) / 2;
+            if(n > m){
+                res -= (n - m) * (n - m + 1)/2;
+            }
+            return res;
+        };
+        ll l = 1 , r = maxSum + 1;
+        while(l < r){
+            ll m = l + ((r-l)>>1);
+            ll sum = 1LL * n;
+            ll left = getSum(m - 1 , index);
+            ll right = getSum(m - 1 , n - index - 1);
+            if(sum + m - 1 + left + right > maxSum) r = m;
+            else l = m + 1;
+        }
+        return l;
+    }
+};
