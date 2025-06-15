40cd2fd17678da3d83fecd87b693f31cff6ef64f
Siddhant Uniyal <110982730+siddhant-uniyal@users.noreply.github.com>
2025-06-15 10:09:44 +0530

    1529. Max Difference You Can Get From Changing an Integer | Beats 0% Time 0% Space

---
+++ greedy/1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxDiff(int num) {
+        string mx = to_string(num);
+        string mn = mx;
+        char from = 'X';
+        for(char &c : mx){
+            if(c != '9' && from == 'X'){
+                from = c;
+            }
+            if(c == from) c = '9';
+        }
+        from = mn[0];
+        for(char &c : mn){
+            if(c == from) c = '1';
+        }
+        return stoi(mx) - stoi(mn);
+    }
+};
