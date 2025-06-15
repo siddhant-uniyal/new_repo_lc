e7f27af5b76e0bd187b89e487076b0ae47f2f00f
Siddhant Uniyal <110982730+siddhant-uniyal@users.noreply.github.com>
2025-06-15 14:50:31 +0530

    440. K-th Smallest in Lexicographical Order | Beats 100% Time 83% Space

---
+++ trie/440-k-th-smallest-in-lexicographical-order/440-k-th-smallest-in-lexicographical-order-0.cpp
@@ -0,0 +1,45 @@
+using ll = long long;
+class Solution {
+public:
+    int findKthNumber(int n, int k) {
+        /*
+        as with all k smallest probs , loop through possible starting and check if poss > k
+
+        at 0 , want to check how many poss for 1_ =>
+        1 
+        10 , 11 ... 19
+        100 , 101 , .. 199
+        
+        so just run a loop for getting poss in o(d)
+        */
+        ll prefix = 0;
+        auto getCnt = [&](ll prefix , ll n) -> ll{
+            ll res = 0;
+            ll prev = prefix;
+            ll curr = prev + 1;
+            while(prev <= n){
+                res += min(n  + 1 , curr) - prev;
+                prev *= 10;
+                curr *= 10;
+            }
+            return res;
+        };
+        while(k){
+            for(ll i = (prefix == 0) ; i <= 9 ; ++i){
+                ll cnt = getCnt(prefix*10 + i , n);
+                if(cnt < k){
+                    k -= cnt;
+                }
+                else{
+                    //choosing 1 from 0
+                    //one poss is consumed by 1
+                    //so --k
+                    --k;
+                    prefix = prefix*10 + i;
+                    break;
+                }
+            }
+        }
+        return prefix;
+    }
+};
