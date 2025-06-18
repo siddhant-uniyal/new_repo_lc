817473514623e74e39be549e9dabfd152579aa49
Siddhant Uniyal <110982730+siddhant-uniyal@users.noreply.github.com>
2025-06-18 21:52:41 +0530

    20. Valid Parentheses | Beats 0% Time 0% Space

---
+++ stack/20-valid-parentheses/20-valid-parentheses-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(char c : s){
+            if(c == '(' || c == '{' || c == '['){
+                st.push(c);
+            }
+            else{
+                char t = st.top();
+                if((t == '(' && c != ')') || (t == '{' && c != '}') || (t == '[' && c != ']')) return false;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
